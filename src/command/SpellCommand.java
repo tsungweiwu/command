4d52fec05566a36d9d47cc62beaffe105f05b2f5
juan-mariscal <mariscal@email.sc.edu>
2020-03-14 13:23:44 -0400

    SpellCommand done

--- src/command/SpellCommand.java
+++ src/command/SpellCommand.java
@@ -1,17 +1,26 @@
 package command;
 
+/**
+ * @author juan-mariscal
+ *
+ */
 public class SpellCommand implements Command {
+	private Document doc;
+	
+	public SpellCommand(Document doc) {
+		this.doc = doc;
+	}
 
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.doc.toString();
 	}
 
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
-
+		this.doc.spell();
 	}
 
 }
