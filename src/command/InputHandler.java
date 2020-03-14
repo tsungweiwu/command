a9b387750ba00962262f6dd532378c353eeb6dfa
juan-mariscal <mariscal@email.sc.edu>
2020-03-14 13:13:10 -0400

    InputHandler done

--- src/command/InputHandler.java
+++ src/command/InputHandler.java
@@ -4,13 +4,31 @@ import java.util.HashMap;
 
 public class InputHandler {
 	private HashMap<String, Command> commands;
+	private LoadCommand loadCommand;
+	private SpellCommand spellCommand;
+	private SaveCommand saveCommand;
+	private PrintCommand printCommand;
 	
 	public InputHandler(Document document) {
-		// todo
+		commands = new HashMap<>();
+		loadCommand = new LoadCommand(document);
+		spellCommand = new SpellCommand(document);
+		saveCommand = new SaveCommand(document);
+		printCommand = new PrintCommand(document);
+		
+		commands.put("load", loadCommand);
+		commands.put("spell", spellCommand);
+		commands.put("save", saveCommand);
+		commands.put("print", printCommand);
 	}
 	
 	public void inputEntered(String data) {
-		// todo
+		if (commands.containsKey(data)) {
+			Command command = commands.get(data);
+			command.execute();
+		} else {
+			System.out.println("Sorry, we don't recognize that command");
+		}
 	}
-
 }
+
